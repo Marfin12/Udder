fbb98f22ab594c2ba6768633d3402c4dd184c099
Marfin Fadhilah <marfin.fadhilah@bankneo.co.id>
2022-11-30 11:49:34 +0700

    add receive warning

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
     
+    override func didReceiveMemoryWarning() {
+        // test
+    }
+
+
 }
 
